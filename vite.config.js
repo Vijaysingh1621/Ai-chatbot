983f48bf59376cdd872e1da4272fd24b84ffce0f
Vijay Singh <rockcodingrookie21@gmail.com>
2024-06-28 11:20:53 +0530

    vite-updated

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['./src/components/Homepage.jsxregenerator-runtime/runtime','antd/es/config-provider/DisabledContext'],
-    }
+  
     
-  },
+  
   plugins: [react()],
 })
