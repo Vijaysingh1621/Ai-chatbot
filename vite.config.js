be9c82145a708b2ce13408a631a9e547a8273c3b
Vijay Singh <rockcodingrookie21@gmail.com>
2024-06-30 01:34:52 +0530

    vite_updated

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- 
-  build: {
-    rollupOptions: {
-      external: ['/src/components/Homepage.jsx/regenerator-runtime/runtime']
-    }
-  },
 
   
     
