59f52d22ee5f1f74d5a1acea58ca52a9d8ce7103
Vijay Singh <rockcodingrookie21@gmail.com>
2024-07-02 00:24:41 +0530

    vite-updated

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // ... other configurations ...
+    resolve: {
+      alias: {
+        'regenerator-runtime/runtime': 'regenerator-runtime/runtime.js',
+      },
+    },
+  };
